e61e0462b6f14c542853cc2d4590ab79d06b279f
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-06-17 13:51:33 +0200

    Implement copy via reflection

--- nodes/src/main/java/de/eldoria/bloodnight/nodes/base/Node.java
+++ nodes/src/main/java/de/eldoria/bloodnight/nodes/base/Node.java
@@ -5,18 +5,25 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import de.eldoria.bloodnight.nodes.MetadataReader;
-import de.eldoria.bloodnight.nodes.NodeContainer;
 import de.eldoria.bloodnight.nodes.base.execution.ExecutableNode;
 import de.eldoria.bloodnight.nodes.base.io.Edge;
 import de.eldoria.bloodnight.nodes.base.io.EditorMeta;
 import de.eldoria.bloodnight.nodes.base.io.InputContainer;
 import de.eldoria.bloodnight.nodes.base.io.OutputContainer;
 import de.eldoria.bloodnight.nodes.base.io.Vec2D;
-import de.eldoria.bloodnight.nodes.serializastion.InputContainerSerializer;
+import de.eldoria.bloodnight.nodes.container.NodeContainer;
+import de.eldoria.bloodnight.nodes.meta.MetadataReader;
+import de.eldoria.bloodnight.nodes.provider.ProviderNode;
+import de.eldoria.bloodnight.nodes.serialization.InputContainerSerializer;
 import de.eldoria.bloodnight.nodes.transform.TransformNode;
 import de.eldoria.bloodnight.nodes.value.ValueNode;
+import org.jetbrains.annotations.ApiStatus;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
@@ -28,7 +35,13 @@ import java.util.function.Consumer;
 @JsonTypeInfo(visible = true, property = "nodeClass", use = JsonTypeInfo.Id.CLASS)
 @JsonPropertyOrder(alphabetic = true)
 @JsonIgnoreProperties({"nodeClass"})
-public sealed abstract class Node permits ExecutableNode, ValueNode, TransformNode {
+public sealed abstract class Node permits ExecutableNode, ProviderNode, TransformNode, ValueNode {
+    @JsonIgnore
+    private MethodHandle constructorHandle;
+    @JsonIgnore
+    private NodeContainer container;
+    @JsonSerialize
+    private int id = -1;
     // Only serialize edges
     @JsonSerialize(using = InputContainerSerializer.class)
     private final InputContainer input;
@@ -72,27 +85,52 @@ public sealed abstract class Node permits ExecutableNode, ValueNode, TransformNo
     /**
      * Get the output container of this node. Calling this will eventually refresh the values provided by the node.
      *
-     * @param container container holding the nodes
      * @return output container
      */
-    public OutputContainer output(NodeContainer container) {
+    public OutputContainer output() {
         return output;
     }
 
-    protected OutputContainer output() {
-        return output;
+    protected final NodeContainer container() {
+        return container;
+    }
+
+    @ApiStatus.Internal
+    public void inject(int id, NodeContainer container) {
+        if (this.id != -1) throw new IllegalStateException("Node id is already set up");
+        this.id = id;
+        this.container = container;
+        input.inject(container);
+    }
+
+    public EditorMeta meta() {
+        return meta;
     }
 
     /**
      * Creates a new instance of this node, which can be used for another mob instance.
      * <p>
      * For optimization reasons, this might be only implemented into nodes which actually hold data. Nodes which do not store any data or just static data might return themselves.
+     * <p>
+     * This node is an uninjected node meaning that the {@link #inject(int, NodeContainer)} method needs to be called afterward.
      *
      * @return a copy of this node if it holds any persistent data.
      */
-    // TODO make this abstract to force implementation or are we fine with the non static data approach?
     public Node copy() {
-        return this;
+        if (constructorHandle == null) {
+            try {
+                var lookup = MethodHandles.publicLookup();
+                MethodType mt = MethodType.methodType(void.class, List.of(Map.class, EditorMeta.class));
+                constructorHandle = lookup.findConstructor(getClass(), mt);
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            return (Node) constructorHandle.invoke(new HashMap<>(input.edges()), meta);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
--- nodes/src/main/java/de/eldoria/bloodnight/nodes/value/ValueNode.java
+++ nodes/src/main/java/de/eldoria/bloodnight/nodes/value/ValueNode.java
@@ -1,19 +1,26 @@
 package de.eldoria.bloodnight.nodes.value;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import de.eldoria.bloodnight.nodes.Fields;
-import de.eldoria.bloodnight.nodes.NodeContainer;
+import de.eldoria.bloodnight.nodes.meta.Fields;
 import de.eldoria.bloodnight.nodes.base.Node;
 import de.eldoria.bloodnight.nodes.base.io.Edge;
 import de.eldoria.bloodnight.nodes.base.io.EditorMeta;
 import de.eldoria.bloodnight.nodes.base.io.OutputContainer;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Base node for nodes providing values which are defined by the user.
  */
 public abstract non-sealed class ValueNode extends Node {
+    private MethodHandle constructorHandle;
 
     public ValueNode(Object value, Map<String, Edge> input, EditorMeta meta) {
         super(input, meta);
@@ -25,12 +32,47 @@ public abstract non-sealed class ValueNode extends Node {
     }
 
     @Override
-    public OutputContainer output(NodeContainer container) {
+    public OutputContainer output() {
         return output();
     }
 
     @JsonProperty
     public Object value() {
         return output().value(Fields.VALUE);
+    }
+
+    @Override
+    public Node copy() {
+        if (constructorHandle == null) {
+            try {
+                Class<?> clazz = null;
+                for (Constructor<?> constructor : getClass().getConstructors()) {
+                    if (constructor.getParameterCount() == 3) {
+                        Parameter found = null;
+                        for (Parameter parameter : constructor.getParameters()) {
+                            JsonProperty annotation = parameter.getAnnotation(JsonProperty.class);
+                            if (annotation.value().equals("value")) {
+                                found = parameter;
+                                break;
+                            }
+                        }
+                        if (found == null) throw new IllegalStateException("No field annotated with value");
+                        clazz = found.getType();
+                        break;
+                    }
+                }
+                if (clazz == null) throw new IllegalStateException("No matching constructor for deserialization");
+                var lookup = MethodHandles.publicLookup();
+                MethodType mt = MethodType.methodType(void.class, List.of(clazz, Map.class, EditorMeta.class));
+                constructorHandle = lookup.findConstructor(getClass(), mt);
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }
+        try {
+            return (Node) constructorHandle.invoke(value(), new HashMap<>(input().edges()), meta());
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 }
